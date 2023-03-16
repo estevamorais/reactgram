ff73a3774fe6be2488cd73a601820b194dd54341
estevamorais <demoraisestevam@gmail.com>
2023-03-16 10:02:31 -0300

    Add more configs to project

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,27 @@
+require('dotenv').config()
+
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
 
-const port = 5000
+const port = process.env.PORT
 
 const app = express()
 
 // config JSON and form data response
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
+
+// solve cors
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }))
+
+// upload directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+// routes
+const router = require('./routes/Router.js')
+
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Running at port ${port}`)
